b29909d2988ead43013d825c6d7dfc744473130a
Luca Cannarozzo <cannarozzoluca@live.it>
2023-08-21 18:36:57 +0200

    feat(gtm): add head and body script

--- src/MediaWikiTagManager.php
+++ src/MediaWikiTagManager.php
@@ -22,12 +22,31 @@ class MediaWikiTagManager
     public static function onBeforePageDisplay(OutputPage $out, Skin $skin)
     {
         $config = MediaWikiServices::getInstance()->getConfigFactory()->makeConfig('mediawikitagmanager');
-        $user = $config->get('MediaWikiTagManagerGTMCode');
+        $GTMCode = $config->get('MediaWikiTagManagerGTMCode');
+        $headScript = <<<MEDIAWIKITAGMANAGER
+        <script>
+        <!-- Google Tag Manager -->
+            <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','{$GTMCode}');</script>
+        <!-- End Google Tag Manager -->
+        </script>
+        MEDIAWIKITAGMANAGER;
+
+        $bodyScript = <<<MEDIAWIKITAGMANAGER
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id={$GTMCode}"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
+        MEDIAWIKITAGMANAGER;
+
         // Add script to <head>
-        $out->addHeadItem('google-tag-manager-head', '<script>/* Your Google Tag Manager head script */</script>');
+        $out->addHeadItem('google-tag-manager-head', $headScript);
 
         // Add script to <body>
-        $out->addInlineScript('google-tag-manager-body', '/* Your Google Tag Manager body script */');
+        $out->addInlineScript('google-tag-manager-body', $bodyScript);
 
         return true;
     }
